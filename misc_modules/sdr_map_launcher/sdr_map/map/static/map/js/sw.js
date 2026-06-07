1dd0bb2b6b5a9794c21199177c0670b038c154b2
Yoann <121693675+F4JTV@users.noreply.github.com>
2026-06-07 10:32:30 +0200

    Add files via upload

--- misc_modules/sdr_map_launcher/sdr_map/map/static/map/js/sw.js
+++ misc_modules/sdr_map_launcher/sdr_map/map/static/map/js/sw.js
@@ -4,7 +4,7 @@
    - Cache des tuiles cartographiques (cache-first) + préchargement de zone.
    Servi depuis '/sw.js' (scope '/') pour intercepter les tuiles cross-origin.
    ========================================================================= */
-const VERSION = "sdrmap-v15";
+const VERSION = "sdrmap-v16";
 const SHELL_CACHE = "shell-" + VERSION;
 const TILE_CACHE = "tiles-" + VERSION;
 
